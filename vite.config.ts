a85c057df12a302c00fea13ccacc632a760db4d0
Kirill20poshliy <qazasd2014@yandex.ru>
2025-06-04 01:27:37 +0300

    feat: pwa has been added

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,64 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        VitePWA({
+              workbox: {
+                globPatterns: [
+                    '**/*.{js,css,html}',
+                    '**/assets/*.{png,jpg,svg,webp}'
+                ],
+                runtimeCaching: [
+                    {
+                        urlPattern: /\.(?:png|jpg|jpeg|svg|webp)$/,
+                        handler: 'CacheFirst',
+                        options: {
+                            cacheName: 'images-cache',
+                            expiration: {
+                                maxEntries: 50,
+                                maxAgeSeconds: 30 * 24 * 60 * 60,
+                            },
+                        },
+                    }
+                ]
+            },
+            registerType: 'autoUpdate',
+            includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+            devOptions: {
+                enabled: true,
+                type: 'module',
+            },
+            strategies: 'generateSW',
+            manifest: {
+                name: 'Rick & Morty App',
+                short_name: 'R&M',
+                theme_color: '#5b5b5b',
+                background_color: "#5b5b5b",
+                display: "standalone",
+                scope: "/",
+                start_url: "/",
+                icons: [
+                    {
+                        src: "web-app-manifest-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                        purpose: "maskable"
+                    },
+                    {
+                        src: "web-app-manifest-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                        purpose: "maskable"
+                    }
+                ],
+            },
+        })
+    ],
     resolve: {
         alias: {
             '@shared': path.resolve(__dirname, './src/shared'),
